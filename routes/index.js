312b6265b977107aeabd22ec23919a02386d4964
Tolya Fam <famovkin@yandex.ru>
2022-04-14 23:25:52 +0300

    feat: create auth middleware

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const auth = require('../middlewares/auth');
 const { register, login } = require('../controllers/users');
 const NotFoundError = require('../errors/notFoundError');
 const routes = express.Router();
 
 routes.post('/signup', express.json(), register);
 routes.post('/signin', express.json(), login);
+routes.use(auth);
 routes.use('*', () => {
   throw new NotFoundError('Страница не найдена. Проверьте URL');
 });
